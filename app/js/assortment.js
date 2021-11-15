46e2717fd7156b8731209ba6007d54f0a2886002
Juli11V <yuliia.demchuk.pm.2020@lpnu.ua>
2021-11-15 10:10:49 +0200

    My site

--- app/js/assortment.js
+++ app/js/assortment.js
@@ -8,4 +8,14 @@ $('.card').on('click',function(){
     $('.tab-content').removeClass('active');
     $('.tab-content').eq(currTab).addClass('active');
 })
+{
+$('.card_2').on('click',function(){
+    let currTab=$(this).parent().index();
 
+    $('.card_2').removeClass('active');
+    $(this).addClass('active');
+
+    $('.tab_content_2').removeClass('active');
+    $('.tab_content_2').eq(currTab).addClass('active');
+})
+}
